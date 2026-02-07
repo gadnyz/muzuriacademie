98dfe71c974be2f864639740bd3049d32b7bf81c
Gadnyz <gadnyumbaiza@gmail.com>
2026-02-07 04:36:18 +0200

    Add FTP deployment workflow

--- home/app/Views/templates/footer.php
+++ home/app/Views/templates/footer.php
@@ -4,15 +4,15 @@
     <div style="margin-bottom: 1rem;">
         <p style="margin: 0.5rem 0;">
             <a href="mailto:contact@muzuriacademie.com"
-                style="color: white; text-decoration: none;">contact@muzuriacademie.com</a>
+                style="color: white; text-decoration: none;">info@muzuriacademie.com</a>
         </p>
-        <p style="margin: 0.5rem 0;">
+        <!-- <p style="margin: 0.5rem 0;">
             <a href="tel:+243998809767"
                 style="color: var(--color-jungle-green); margin-right: 1rem; text-decoration: none; font-weight: bold;">+243
                 998 809 767</a>
             <a href="tel:+243994230865"
                 style="color: var(--color-jungle-green); text-decoration: none; font-weight: bold;">+243 994 230 865</a>
-        </p>
+        </p> -->
     </div>
     <div>
         &copy; 2026 Muzuri Académie. Tous droits réservés.
